c4067243f7b476a4131b3a1b51b2b15da52c717d
Arun Kumar <arunpasupathi24@gmail.com>
2024-07-14 16:36:12 +1000

    feat: integrated stackoverflow api

---
+++ layers/api/stackoverflow/constants.py
@@ -0,0 +1,5 @@
+STACKOVERFLOW_API_VERSION = 2.3
+
+STACKOVERFLOW_API_BASE_URL = "https://api.stackexchange.com"
+
+SUPPORTED_QUERIES = ["key", "site", "order", "sort", "filter", "page"]
---
+++ layers/api/stackoverflow/index.py
@@ -0,0 +1,29 @@
+from typing import Union
+from fetch.get import get
+from .types import QuestionsResponse
+
+from .utils import base_url_for, supported_query_params
+
+
+class StackOverflowApi:
+    def __init__(self, key) -> None:
+        self.api_key = key
+
+    def fetch_questions(self, **kwargs) -> Union[QuestionsResponse, None]:
+
+        try:
+            api_url_for_questions = base_url_for(params="questions")
+
+            queries = {"key": self.api_key, **kwargs}
+
+            questions_resp = get(
+                url=api_url_for_questions,
+                params=supported_query_params(query_params=queries),
+            )
+
+            if questions_resp["status"] == "success":
+                return questions_resp["response"]
+
+            return None
+        except Exception as e:
+            raise e
