bf3157a2847d144592eddd7e36819fe95d20c69c
Arun Kumar <arunpasupathi24@gmail.com>
2024-08-22 22:36:30 +1000

    chore: update get call to verify response json error body

--- src/helpers/fetch/get.spec.ts
+++ src/helpers/fetch/get.spec.ts
@@ -8,24 +8,34 @@ describe("get", () => {
       status: 200,
     } as unknown as Response);
 
-    const response = await get("http://mockurl.com", {});
+    const response = await get("http://mockurl.com");
 
     expect(response).toEqual({
-      statusCode: 200,
       data: "sample response",
+      ok: true,
+      status: 200,
     });
   });
 
-  it("should throw error when 'ok' = false", async () => {
+  it("should throw stackoverflow error when 'ok' = false", async () => {
     jest.spyOn(global, "fetch").mockResolvedValueOnce({
-      json: jest.fn().mockResolvedValue("sample response"),
+      json: jest.fn().mockResolvedValue({
+        error_id: 400,
+        error_name: "bad_parameter",
+        error_message: "invalid parameter passed",
+      }),
       ok: false,
     } as unknown as Response);
 
     try {
-      await get("http://mockurl.com", {});
+      await get("http://mockurl.com");
     } catch (error) {
-      expect(error.message).toBe("Error in get api");
+      expect(error.name).toBe("stackoverflow-error");
+      expect(error.errorResponse).toEqual({
+        error_id: 400,
+        error_name: "bad_parameter",
+        error_message: "invalid parameter passed",
+      });
     }
   });
 });
--- src/helpers/fetch/get.ts
+++ src/helpers/fetch/get.ts
@@ -1,21 +1,38 @@
+import { StackOverflowError } from "../errors";
 import { GetResponse } from "../../types/helpers/fetch";
+import { StackOverflowErrorResponse } from "../../types/helpers/error";
 
 export const get = async <T>(
   url: string,
   headers?: Record<string, string>
 ): Promise<GetResponse<T>> => {
-  try {
+  const getVendor = async (): Promise<GetResponse<T>> => {
     const response = await fetch(url, {
+      method: "GET",
       headers,
     });
-
     const data = await response.json();
 
     return {
-      statusCode: response.status,
       data,
+      ok: response.ok,
+      status: response.status,
     };
-  } catch (error) {
-    throw new Error("Error in get api");
-  }
+  };
+
+  return getVendor()
+    .then((response) => {
+      if (!response.ok) {
+        throw new StackOverflowError(
+          response.data as StackOverflowErrorResponse
+        );
+      }
+
+      return response;
+    })
+    .catch((error) => {
+      console.log(error);
+      //TODO: check for internal/500 errors
+      throw error;
+    });
 };
