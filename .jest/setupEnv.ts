694378c9fb7017d33985704dd05fa28bae216b06
Arun Kumar <arunpasupathi24@gmail.com>
2024-08-24 21:04:56 +1000

    chore: add docker compose for testing

--- .jest/setupEnv.ts
+++ .jest/setupEnv.ts
@@ -1,4 +1,4 @@
-process.env.STACK_API_URL = "http://localhost:4000";
+process.env.STACK_API_URL = "http://0.0.0.0:3000";
 process.env.STACK_API_KEY = "U4DMV*8nvpm3EOpvf69Rxw((";
 process.env.OVERFLOWDESK_DB_HOST = "0.0.0.0";
 process.env.OVERFLOWDESK_DB_NAME = "postgres";
