d3835f5bbb115ac0b19189a77271c2bc1b7d8c2e
Arun Kumar <arunpasupathi24@gmail.com>
2024-08-24 14:59:06 +1000

    chore: added faker for unit test purpose

---
+++ src/helpers/faker/common.ts
@@ -0,0 +1,9 @@
+import { faker } from "@faker-js/faker";
+
+export const generateRandomIntNumber = (): number => {
+  return faker.number.int({ max: 1000 });
+};
+
+export const generateRandomLines = (length: number = 1): string => {
+  return faker.lorem.lines({ min: length, max: length });
+};
