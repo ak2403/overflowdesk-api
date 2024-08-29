be7804af72f504d8bc33054f932fab52944c1432
Arun Kumar <arunpasupathi24@gmail.com>
2024-08-29 13:38:19 +1000

    chore: update main.yml and db config with local config

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,11 +4,11 @@ import { StackDbProps } from "../types/common/config";
 dotenv.config();
 
 export const getStackDbProps = (): StackDbProps => {
-  const host = process.env.OVERFLOWDESK_DB_HOST || "";
-  const name = process.env.OVERFLOWDESK_DB_NAME || "";
-  const username = process.env.OVERFLOWDESK_DB_USERNAME || "";
-  const password = process.env.OVERFLOWDESK_DB_PASSWORD || "";
-  const port = process.env.OVERFLOWDESK_DB_PORT || 0;
+  const host = process.env.OVERFLOWDESK_DB_HOST || "0.0.0.0";
+  const name = process.env.OVERFLOWDESK_DB_NAME || "postgres";
+  const username = process.env.OVERFLOWDESK_DB_USERNAME || "admin";
+  const password = process.env.OVERFLOWDESK_DB_PASSWORD || "secret";
+  const port = process.env.OVERFLOWDESK_DB_PORT || 5430;
 
   return {
     host,
